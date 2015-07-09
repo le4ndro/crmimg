c9024345c4804bace2368b16c5df37ba899747de
Leandro Souza <leandrosabreu@gmail.com>
2015-07-09 12:43:33 -0300

    Configuração do rspec e guard.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'database_cleaner'
+require 'capybara/rspec'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.order = "random"
+  config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end      
+end
