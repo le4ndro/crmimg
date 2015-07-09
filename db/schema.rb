67a192112aaec82c5b0e82b358cb2cfc4869db17
Leandro Souza <leandrosabreu@gmail.com>
2015-07-09 12:20:35 -0300

    Mudança de banco para postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,66 +13,69 @@
 
 ActiveRecord::Schema.define(version: 20150703015740) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "atividades", force: :cascade do |t|
-    t.string   "nome",            limit: 255
-    t.string   "tipo",            limit: 255
-    t.text     "descricao",       limit: 65535
-    t.text     "observacao",      limit: 65535
-    t.text     "acompanhamento",  limit: 65535
-    t.string   "status",          limit: 255
-    t.integer  "oportunidade_id", limit: 4
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "nome"
+    t.string   "tipo"
+    t.text     "descricao"
+    t.text     "observacao"
+    t.text     "acompanhamento"
+    t.string   "status"
+    t.integer  "oportunidade_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "atividades", ["oportunidade_id"], name: "index_atividades_on_oportunidade_id", using: :btree
 
   create_table "cliente_potencials", force: :cascade do |t|
-    t.string   "nome",       limit: 255
-    t.text     "descricao",  limit: 65535
-    t.string   "website",    limit: 255
-    t.text     "observacao", limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "nome"
+    t.text     "descricao"
+    t.string   "website"
+    t.text     "observacao"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "contatos", force: :cascade do |t|
-    t.string   "nome",                 limit: 255
-    t.string   "cargo",                limit: 255
-    t.string   "telefone",             limit: 255
-    t.string   "celular",              limit: 255
-    t.string   "email",                limit: 255
-    t.string   "facebook",             limit: 255
-    t.string   "linkedin",             limit: 255
-    t.string   "googleplus",           limit: 255
-    t.integer  "cliente_potencial_id", limit: 4
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.string   "nome"
+    t.string   "cargo"
+    t.string   "telefone"
+    t.string   "celular"
+    t.string   "email"
+    t.string   "facebook"
+    t.string   "linkedin"
+    t.string   "googleplus"
+    t.integer  "cliente_potencial_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   add_index "contatos", ["cliente_potencial_id"], name: "index_contatos_on_cliente_potencial_id", using: :btree
 
   create_table "oportunidades", force: :cascade do |t|
-    t.string   "nome",                 limit: 255
-    t.string   "descricao",            limit: 255
-    t.text     "observacao",           limit: 65535
-    t.string   "status",               limit: 255
-    t.decimal  "receita_estimada",                   precision: 10
-    t.string   "probabilidade",        limit: 255
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.integer  "cliente_potencial_id", limit: 4
+    t.string   "nome"
+    t.string   "descricao"
+    t.text     "observacao"
+    t.string   "status"
+    t.decimal  "receita_estimada"
+    t.string   "probabilidade"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "cliente_potencial_id"
   end
 
   add_index "oportunidades", ["cliente_potencial_id"], name: "index_oportunidades_on_cliente_potencial_id", using: :btree
 
   create_table "produtos", force: :cascade do |t|
-    t.string   "nome",       limit: 255
-    t.text     "descricao",  limit: 65535
-    t.decimal  "preco",                    precision: 10
-    t.text     "obs",        limit: 65535
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
+    t.string   "nome"
+    t.text     "descricao"
+    t.decimal  "preco"
+    t.text     "obs"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "atividades", "oportunidades"
